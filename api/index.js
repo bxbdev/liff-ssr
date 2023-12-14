2b0f2342604c22a7f45613f9cbee1fae613bcb9c
Fiona <workartsources@gmail.com>
2023-12-14 23:15:40 +0800

    added vercel settings

---
+++ api/index.js
@@ -0,0 +1,3 @@
+const server = require("../dist/liff/server/main");
+
+module.exports = server.app();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
+import { APP_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    {
+      provide: APP_ID,
+      useValue: 'server-app',
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
